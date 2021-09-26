1f0f6f2c32024bfc3a428830088b232618e29a96
Alican Uver <alican.uver@emlakjet.com>
2021-09-26 19:31:25 +0300

    Add search bar component

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { View, Text, SafeAreaView } from 'react-native';
 import HeaderTabs from '../src/components/HeaderTabs';
+import SearchBar from '../src/components/SearchBar';
 
 const Home = () => {
   return (
     <SafeAreaView style={{ backgroundColor: '#eee', height: '100%' }}>
       <View style={{ backgroundColor: 'white', padding: 15 }}>
         <HeaderTabs />
+        <SearchBar />
       </View>
     </SafeAreaView>
   )
